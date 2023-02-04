736bf57aa24c3dac81979458e6b3a07a75b86721
kaito0311 <tanminh8b@gmail.com>
2023-02-04 08:12:11 +0700

    revert fail 1

--- lib/friend/list_friend.dart
+++ lib/friend/list_friend.dart
@@ -39,6 +39,7 @@ class History extends StatelessWidget {
             Container(
               height: 40,
               color: Colors.white,
+           
               child: Row(
                 children: [
                   Align(
@@ -56,18 +57,20 @@ class History extends StatelessWidget {
                       ),
                     ),
                   ),
+
                   Expanded(
                     child: Align(
                       alignment: Alignment.centerRight,
                       child: Container(
+
                         child: Padding(
                           padding: const EdgeInsets.fromLTRB(8, 8, 16, 8),
                           child: Text(
                             'Sắp xếp',
                             style: TextStyle(
-                              color: Colors.blue,
-                              fontSize: 14,
-                            ),
+                                color: Colors.blue,
+                                fontSize: 14,
+                                ),
                           ),
                         ),
                       ),
@@ -95,22 +98,19 @@ class _MyListPageState extends State<MyListPage> {
   List<String> data = ["1", "2", "3", "4"];
   get_list_blocks() async {
     var response = await http.post(
-      Uri.parse(
-        "http://184.169.213.180:3000/it4788/friend/get_list_blocks/",
-      ),
+      Uri.parse("http://184.169.213.180:3000/it4788/friend/get_list_blocks/",),
       headers: <String, String>{
         'Content-Type': 'application/json; charset=UTF-8',
       },
       body: jsonEncode(<String, String>{
         'token':
             "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzYzNiZDUyOTgxNTJmZjUzYjI2MDgwNSIsImRhdGVMb2dpbiI6IjIwMjMtMDItMDFUMTI6NTc6MTYuODM3WiIsImlhdCI6MTY3NTI1NjIzNywiZXhwIjoxNjg1MjU2MjM2fQ.AafjbzuumSE915Om3JoD1qLuyhJbtVJDD1A9aLVHpBc",
-        "index": "0",
-        "count": "5",
+        "index":"0",
+        "count": "5", 
       }),
     );
     print(response.body);
   }
-
   @override
   Widget build(BuildContext context) {
     get_list_blocks();
@@ -120,7 +120,7 @@ class _MyListPageState extends State<MyListPage> {
       itemCount: data.length,
       itemBuilder: ((context, index) {
         return Card(
-            // color: Color.fromARGB(255, 230, 227, 227),
+            color: Color.fromARGB(255, 230, 227, 227),
             child: Padding(
                 padding: const EdgeInsets.all(8),
                 child: ListTile(
@@ -136,107 +136,26 @@ class _MyListPageState extends State<MyListPage> {
                     // backgroundColor: Colors.white,
                   ),
                   trailing: Container(
-                    width: 20,
+                    width: 80,
                     child: Row(
                       children: [
                         Expanded(
-                            child: IconButton(
-                          icon: Icon(Icons.more_horiz),
-                          onPressed: () {
-                            print("show pop up of this friends");
-                            _showMoreOption(context);
-                          },
-                        )),
+                          child: TextButton(
+                            style: TextButton.styleFrom(
+                              textStyle: const TextStyle(fontSize: 14),
+                            ),
+                            onPressed: () {},
+                            child: const Text(
+                              'Bỏ chặn',
+                              style: TextStyle(color: Colors.grey),
+                            ),
+                          ),
+                        ),
                       ],
                     ),
                   ),
                 )));
       }),
-    );
-  }
-
-  _showMoreOption(context) {
-    showModalBottomSheet(
-      context: context,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(10.0),
-      ),
-      builder: (BuildContext context) {
-        return Container(
-          height: 200,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10),
-          ),
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                Container(
-                  padding: EdgeInsets.all(10.0),
-                  child: Row(
-                    children: <Widget>[
-                      Icon(
-                        Icons.chat,
-                        color: Colors.black,
-                      ),
-                      SizedBox(
-                        width: 10.0,
-                      ),
-                      Text(
-                        'Nhắn tin',
-                        style: TextStyle(fontSize: 18.0),
-                      )
-                    ],
-                  ),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Container(
-                  padding: EdgeInsets.all(10.0),
-                  child: Row(
-                    children: <Widget>[
-                      Icon(
-                        Icons.block,
-                        color: Colors.black,
-                      ),
-                      SizedBox(
-                        width: 10.0,
-                      ),
-                      Text(
-                        'Chặn',
-                        style: TextStyle(fontSize: 18.0),
-                      )
-                    ],
-                  ),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Container(
-                  padding: EdgeInsets.all(10.0),
-                  child: Row(
-                    children: <Widget>[
-                      Icon(
-                        Icons.person_remove_outlined,
-                        color: Colors.red,
-                      ),
-                      SizedBox(
-                        width: 10.0,
-                      ),
-                      Text(
-                        'Hủy kết bạn',
-                        style: TextStyle(fontSize: 18.0, color: Colors.red),
-                      )
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
-        );
-      },
     );
   }
 }
