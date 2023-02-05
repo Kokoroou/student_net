d595685c2a97f70767b8ce02391702b5374d61d5
dnh1704 <huanhuanhuank56a1@gmail.com>
2023-02-06 00:23:15 +0700

    like bai viet

--- lib/pages/main_app/home_page.dart
+++ lib/pages/main_app/home_page.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_icons/flutter_icons.dart';
+import 'package:http/http.dart';
 
 import 'package:student_net/models/newfeed_model.dart';
 import 'package:student_net/pages/main_app/root_app.dart';
@@ -17,9 +20,33 @@ class _HomePageState extends State<HomePage> {
   // Postfeed a = Postfeed('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzYzU2NGJjOTgxNTJmZjUzYjI2MDgxMyIsImRhdGVMb2dpbiI6IjIwMjMtMDEtMzFUMTc6MzU6NDUuMjQxWiIsImlhdCI6MTY3NTE4NjU0NSwiZXhwIjoxNjg1MTg2NTQ0fQ.U1LIKoaK7Szczs0cHFZ4STJ9nWqC4jZxO_ZwoEwFW-E', 50);
 
   static List cleanPostList = [];
+  static const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzYzU2NGJjOTgxNTJmZjUzYjI2MDgxMyIsImRhdGVMb2dpbiI6IjIwMjMtMDEtMzFUMTc6MzU6NDUuMjQxWiIsImlhdCI6MTY3NTE4NjU0NSwiZXhwIjoxNjg1MTg2NTQ0fQ.U1LIKoaK7Szczs0cHFZ4STJ9nWqC4jZxO_ZwoEwFW-E';
   Postfeed a = Postfeed(
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzYzU2NGJjOTgxNTJmZjUzYjI2MDgxMyIsImRhdGVMb2dpbiI6IjIwMjMtMDEtMzFUMTc6MzU6NDUuMjQxWiIsImlhdCI6MTY3NTE4NjU0NSwiZXhwIjoxNjg1MTg2NTQ0fQ.U1LIKoaK7Szczs0cHFZ4STJ9nWqC4jZxO_ZwoEwFW-E',
+  token,
       50);
+
+
+  likePost(id, token) async {
+    print("tesst like");
+    String url = 'http://184.169.213.180:3000/it4788/like/like?token=' + token.toString() + '&id=' + id.toString();
+    final uri = Uri.parse(url);
+    final headers = {'Content-Type': 'application/json'};
+    Map<String, dynamic> body = {
+      'token': token,
+      'id': id.toString(),
+    };
+
+    String jsonBody = json.encode(body);
+    final encoding = Encoding.getByName('utf-8');
+    Response response = await post(
+      uri,
+      headers: headers,
+      body: jsonBody,
+      encoding: encoding,
+    );
+
+
+  }
 
   cleanData() async {
     cleanPostList = await a.PostList;
@@ -183,9 +210,11 @@ class _HomePageState extends State<HomePage> {
                                 ],
                               ),
                               Row(
+
                                 mainAxisAlignment:
                                     MainAxisAlignment.spaceAround,
                                 children: [
+
                                   Container(
                                     width: 90,
                                     height: 35,
@@ -194,19 +223,28 @@ class _HomePageState extends State<HomePage> {
                                         color: const Color(0xFFE5E5E5)
                                             .withOpacity(0.5)),
                                     child: Row(
+                                      
                                       mainAxisAlignment:
                                           MainAxisAlignment.spaceEvenly,
                                       children: [
-                                        const Icon(
-                                          Feather.heart,
-                                          color: white,
-                                          size: 17,
+
+                                         IconButton(
+                                           icon: const Icon(
+                                             Feather.heart,
+                                             color: white,
+                                             size: 17,),
+                                           onPressed: () {
+                                             likePost(cleanPostList[index]['id'],token);
+
+                                         },
+
                                         ),
                                         Text(
                                           cleanPostList[index]['like'],
                                           style: const TextStyle(
                                               fontSize: 17, color: white),
-                                        )
+                                        ),
+
                                       ],
                                     ),
                                   ),
